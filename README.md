34378d53b84ed4fc43aeea856a9501f638dfab82
Gilbert Vrancken <vrancken.gilbert@gmail.com>
2018-04-25 14:35:44 +0200

    using scrambler and reflector as described on wiki

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Visual cyphering/decyphering in Unity
+
+
+Material
+
+* https://www.codesandciphers.org.uk/enigma/example1.htm
+* https://en.wikipedia.org/wiki/Reflector_(cipher_machine)
+* https://en.wikipedia.org/wiki/Enigma_machine
--- unity/Assets/Main.cs
+++ unity/Assets/Main.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Main : MonoBehaviour
 {
 
-    string input = "AAAAA";
+    string alphabet = "ABCDEF";
 
-    string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    int[] s1 = new int[] { 0, 0, 0, 0, 0 };
+    string input = "AAAAAAAAA";
+    
+    // scramblers:
+    int[] s1 = new int[] { 0, 1, 2, 3, 4, 5 }; 
 
+    // Reflector is a mirror, so very important that e.g. 0->2 then 2->0!
+    // NB this ensures a letter is never itself when encrypted which is a major flaw in this system.
+    int[] reflector = new int[] { 2, 3, 0, 1, 5, 4 }; 
+
+    // use an index to fake slide left/right
     int s1index = 0;
 
     void Start()
@@ -17,20 +24,21 @@ public class Main : MonoBehaviour
         for (int i = 0; i < input.Length; ++i)
         {
             char it = input[i];
-            int alphabetIndex = alphabet.IndexOf(it);
-            int indexAfterS1 = s1[alphabetIndex] + s1index;
-            
-            // if scrambler has more elements than alphabet
-            indexAfterS1 = indexAfterS1 % alphabet.Length;
-            
+            int indexInput = alphabet.IndexOf(it);
+            // scramblers
+            int indexS1 = (s1[indexInput] + s1index) % s1.Length;
+            // reflector
+            int indexS1R1 = reflector[indexS1];
+            // walk back up from reflector
+            int indexS1R1S1 = s1[indexS1R1];
+
             // output value
-            Debug.Log(alphabet[indexAfterS1]);
-            
-            // slide scrambler one left for next turn
+            Debug.Log(alphabet[indexS1R1S1]);
+
+            // slide scrambler one left for next turn, if we reached end, start over.
             s1index++;
-            
-            // if we reached end, start over.
-            s1index = s1index % s1.Length;
+            s1index = (s1index + s1.Length) % s1.Length;
+
         }
     }
 
